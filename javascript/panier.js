acdf0b53e9336536d54c710e72815ef5e2c4eede
Kerstern <baptiste.franel@gmail.com>
2021-03-10 09:03:22 +0100

    Update panier.js
    - Ajout des RegEx pour validation des données

--- javascript/panier.js
+++ javascript/panier.js
@@ -11,13 +11,13 @@ function AffPanier(longueur) {
         var i = 0;
         do {
             getApi('GET', 'http://localhost:3000/api/teddies/' + panier[i])
-            .then(function (e) {
-                teddies = JSON.parse(e.target.response);
-                getTeddies(teddies);
-                calcTotal(teddies, longueur);
-            }, function (e) {
-                console.log("Une erreur est survenu dans la récupération des données.")
-            });
+                .then(function (e) {
+                    teddies = JSON.parse(e.target.response);
+                    getTeddies(teddies);
+                    calcTotal(teddies, longueur);
+                }, function (e) {
+                    console.log("Une erreur est survenu dans la récupération des données.")
+                });
             i++
         } while (longueur != i)
     } else {
@@ -64,13 +64,24 @@ function sendCommand(event) {
         city: document.getElementById('ville').value,
         email: document.getElementById('email').value
     }
+    let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let regexString = /^[a-zA-Z]*$/;
+    let regexAddress = /^[a-z0-9\s,'-]*$/i;
 
     if (products == null || products == undefined) {
         alert("Le panier est vide");
     }
 
-    if(contact.firstName == "" || contact.lastName == "" || contact.address == "" || contact.city == "" || contact.email == "") {
+    if (contact.firstName == "" || contact.lastName == "" || contact.address == "" || contact.city == "" || contact.email == "") {
         alert("Veuillez remplir les champs obligatoires (*)")
+    } else if (!contact.email.match(regexEmail)) {
+        alert("Le format de l'addresse mail est incorrect")
+    } else if (!contact.lastName.match(regexString)) {
+        alert("Le format du nom est incorrect")
+    } else if (!contact.firstName.match(regexString)) {
+        alert("Le format du prénom est incorrect")
+    } else if (!contact.city.match(regexString)) {
+        alert("Le format de la ville est incorrect")
     } else {
         if (typeof (products) == "object" && typeof (contact) == "object") {
             fetch(new Request('http://localhost:3000/api/teddies/order', {
@@ -83,14 +94,14 @@ function sendCommand(event) {
                 body: JSON.stringify({ contact, products }),
             })
                 .then(response => response.json())
-                .then(json => {localStorage.setItem('order', JSON.stringify(json)), window.location.href= "remerciements.html#"+json.orderId})
+                .then(json => { localStorage.setItem('order', JSON.stringify(json)), window.location.href = "remerciements.html#" + json.orderId })
                 .catch(e => console.error)
         } else {
             console.log("Mauvais type de données.")
         }
     }
 
-    
+
 }
 
 
